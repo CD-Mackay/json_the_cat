3b30a208d517247dde9d401fcaa946471a94d073
CD-Mackay <connor.mackay@gmail.com>
2020-12-03 20:16:31 +0000

    refactored to use index.js

--- breedfetcher.js
+++ breedfetcher.js
@@ -1,29 +1,27 @@
 const request = require('request');
-const breedName = process.argv.slice(2);
-let url = 'https://api.thecatapi.com/v1/breeds/search?q=';
-let query = breedName.join("").toLowerCase().slice(0, 3);
-url += query;
 
-request(url, function(error, response, body) {
-  if (error) {
-    console.log("Something has gone awry, heres a clue: ", error.code);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("The breed does not exist");
-    return;
-  } else {
-    console.log(data[0].description);
-  
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  let url = 'https://api.therealcatapi.com/v1/breeds/search?q=';
+  let query = breedName.toLowerCase().slice(0, 3);
+  url += query;
+  request(url, function(error, response, body) {
+    if (error) {
+      callback(error.code, null);
+    } else {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        let error = "Breed does not exist, try a real cat";
+        callback(error, null);
+      } else {
+        callback(null, data[0].description);
+      }
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
 
 
 
 
-// request('https://api.thecatapi.com/v1/breeds/search?q=sib', function(error, response, body) {
-//   const data = JSON.parse(body);
-//   console.log(data[0].description);
-//   console.log(typeof data);
-// })
+
